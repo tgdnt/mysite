c16e293cc8b22feefc56afcfbe10f4043dc3acea
Tiago Donato <tgdnt@pm.me>
2019-01-09 17:44:48 -0600

    Enhances .org-jekyll.sh
    * Uses sed to turn org code blocks into Jekyll highlighted code
      blocks via {% highlight %}.
    * Abandons compress.html layout because it breaks code blocks with
      line numbers.
    * Does not use line numbers anyway, though, because it looks ugly.

--- .org-jekyll.sh
+++ .org-jekyll.sh
@@ -1,6 +1,10 @@
 rm -rf _posts/*
-find . -name "*.org" | while read -r filename; do
+find ./.orgsource -name "*.org" | while read -r filename; do
+    sed -e 's/\(\#\+BEGIN\_SRC \)\(.*\)/\#\+BEGIN_HTML\
+{\% highlight \2 \%}/' -e 's/\#\+END\_SRC/{\% endhighlight \%}\
+\#\+END_HTML/' $filename > $filename.temp.org
     newfilename=$(echo $filename | sed -e "s_\(\.\/\)\(\.orgsource\/\)\(.*\)\(.org\)_\1\3\.html_g")
-    pandoc -t html -o $newfilename $filename
+    pandoc -t html -o $newfilename $filename.temp.org
+    rm $filename.temp.org
 done
 jekyll $1
