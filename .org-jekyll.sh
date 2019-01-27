7c3d58945df71874eb1f86c2e4b0620f75bfe9a3
Tiago Donato <tgdnt@pm.me>
2019-01-26 22:43:02 -0600

    Moves .orgsource to _orgsource

--- .org-jekyll.sh
+++ .org-jekyll.sh
@@ -1,9 +1,9 @@
 rm -rf _posts/*
-find ./.orgsource -name "*.org" | while read -r filename; do
+find ./_orgsource -name "*.org" | while read -r filename; do
     sed -e 's/\(\#\+BEGIN\_SRC \)\(.*\)/\#\+BEGIN_HTML\
 {\% highlight \2 \%}/' -e 's/\#\+END\_SRC/{\% endhighlight \%}\
 \#\+END_HTML/' $filename > $filename.temp.org
-    newfilename=$(echo $filename | sed -e "s_\(\.\/\)\(\.orgsource\/\)\(.*\)\(.org\)_\1\3\.html_g")
+    newfilename=$(echo $filename | sed -e "s/\(\.\/\)\(\_orgsource\/\)\(.*\)\(.org\)/\1\3\.html/g")
     pandoc -t html -o $newfilename $filename.temp.org
     rm $filename.temp.org
 done
