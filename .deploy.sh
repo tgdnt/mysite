d655891c99589d7e729dca5b0378d66e540a7643
Tiago D. A. de Araujo <tgdnt@pm.me>
2020-07-18 16:57:30 -0500

    Updated .deploy.sh to run manually

--- .deploy.sh
+++ .deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-aws s3 sync _site/ s3://tgdnt.com --acl public-read --delete
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id E21COGIAQQET3R --paths '/*'
+aws s3 sync _site/ s3://tgdnt.com --acl public-read --delete --profile tgdnt.com
+aws cloudfront create-invalidation --distribution-id E21COGIAQQET3R --paths '/*' --profile tgdnt.com
